f0a3e0bb9408b82ec4f79d2024b7613df8f5ac87
unknown <bintangrahore@gmail.com>
2024-02-29 14:08:04 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:project_android_studio/column_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: "Aplikasi Flutter Pertama",
+      home: ColumnWidget(),
+    );
+  }
+}
